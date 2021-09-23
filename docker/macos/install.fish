2d9e9e9299180bb6919ad273a77b463584cd7b88
stakme <stak.me.g@gmail.com>
2021-09-23 13:23:17 +0900

    install script

---
+++ docker/macos/install.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+mkdir -p ~/.docker/bin;
+mkdir -p ~/.docker/cli-plugins;
+
+# docker
+# https://docs.docker.com/engine/install/binaries/#install-client-binaries-on-macos
+curl -OL https://download.docker.com/mac/static/stable/x86_64/docker-20.10.8.tgz;
+tar -xzvf docker-20.10.8.tgz;
+rm docker-20.10.8.tgz;
+sudo xattr -rc docker;
+mv docker/docker ~/.docker/bin;
+mv docker/cli-plugins/* ~/.docker/cli-plugins/;
+
+# docker compose
+curl -OL https://github.com/docker/compose-cli/releases/download/v2.0.0-rc.2/docker-compose-darwin-amd64
+chmod +x docker-compose-darwin-amd64;
+mv docker-compose-darwin-amd64 ~/.docker/bin/cli-plugins/docker-compose;
+
