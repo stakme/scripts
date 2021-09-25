d958006cd22f12986f96944d337468971cc1a4c6
stakme <stak.me.g@gmail.com>
2021-09-25 10:18:23 +0900

    update install script

--- docker/macos/install_docker.fish
+++ docker/macos/install_docker.fish
@@ -1,27 +1,16 @@
 #!/usr/bin/env fish
 
-cd $TMPDIR;
-
-mkdir -p ~/.docker/bin;
-mkdir -p ~/.docker/cli-plugins;
-
 # docker
 # https://docs.docker.com/engine/install/binaries/#install-client-binaries-on-macos
-curl -OL https://download.docker.com/mac/static/stable/x86_64/docker-20.10.8.tgz;
-tar -xzvf docker-20.10.8.tgz;
-rm docker-20.10.8.tgz;
-# sudo xattr -rc docker;
-mv docker/docker ~/.docker/bin;
-mv docker/cli-plugins/* ~/.docker/cli-plugins/;
+brew install docker;
+mkdir -p ~/.docker/cli-plugins;
 
 # docker compose
-# https://github.com/docker/compose-cli
-curl -OL https://github.com/docker/compose-cli/releases/download/v2.0.0-rc.2/docker-compose-darwin-amd64
-chmod +x docker-compose-darwin-amd64;
-mv docker-compose-darwin-amd64 ~/.docker/cli-plugins/docker-compose;
+# https://github.com/docker/compose
+curl -L -o ~/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.0.0-rc.3/docker-compose-darwin-amd64
+chmod +x ~/.docker/cli-plugins/docker-compose;
 
-which docker;
-if [ "$status" = "1" ]
-    echo "set PATH \$HOME/.docker/bin \$PATH" >> ~/.config/fish/config.fish;
-    source ~/.config/fish/config.fish;
-end
+# docker buildx
+# https://github.com/docker/buildx
+curl -L -o ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.darwin-amd64
+chmod +x ~/.docker/cli-plugins/docker-buildx;
